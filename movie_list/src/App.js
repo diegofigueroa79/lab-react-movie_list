d82498f77e8611034459fa63d782767305260838
diegofigueroa79 <diegofigueroa79@gmail.com>
2020-06-10 01:50:17 -0700

    added button function and set state function

--- movie_list/src/App.js
+++ movie_list/src/App.js
@@ -1,12 +1,34 @@
 import React from 'react';
 import './App.css';
+import './data/movies.js';
 
 class App extends React.Component {
+
+  state = {
+    WelcomeText : "What's up"
+  }
+
+  greeting() {
+    return this.state.WelcomeText;
+  };
+
+  changeText() {
+    console.log('sup')
+    return this.setState({
+      WelcomeText : "Goodbye"
+    })
+  }
+
   render() {
+  
     return (
+      
+      
+  
       <div className="App">
         <header className="App-header">
-          <h1>Hello World!</h1>
+          <h1>{this.greeting()}</h1>
+          <button onClick={this.changeText}>Change Banner</button>
         </header>
       </div>
     );
